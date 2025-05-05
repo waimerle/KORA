6ddc634b9b10312ea6a9ea5f32620d8905b93ff1
waimerle <167164782+waimerle@users.noreply.github.com>
2025-05-05 14:31:12 +0200

    Update views.py

--- views.py
+++ views.py
@@ -1,9 +1,17 @@
 import random
 import csv
 import os
+import json
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
+
+codeCSV = '/var/www/django-projekt/KORA/Code.csv'
+gemeindenCSV = '/var/www/django-projekt/KORA/Gemeinden.csv'
+mitarbeiterChipCSV = '/var/www/django-projekt/KORA/MitarbeiterChip.csv'
+räumeCSV = '/var/www/django-projekt/KORA/Raum.csv'
+rfidCSV = '/var/www/django-projekt/KORA/RFID.csv'
+temperaturCSV = '/var/www/django-projekt/KORA/Temperatur.csv'
 
 # Funktion um einfaches Passwort zu erstellen
 def einfachesPasswortErstellen(vergebenePasswörter, länge=6):
@@ -26,12 +34,11 @@ def adminPasswortErstellen(vergebenePasswörter, länge=10):
 
 # Gemeinden / Stadtverwaltungen aus CSV laden
 def gemeindenLaden():
-    csvPfad = os.path.join(settings.BASE_DIR, '/var/www/django-projekt/KORA/Gemeinden.csv')
     gemeinden = []
 
-    if os.path.isfile(csvPfad):
-        with open(csvPfad, mode="r", encoding="utf-8-sig") as datei:
-            reader = csv.DictReader(datei, delimiter=";")
+    if os.path.isfile(gemeindenCSV):
+        with open(gemeindenCSV, mode="r", encoding="utf-8-sig") as datei:
+            reader = csv.DictReader(datei)
             for zeile in reader:
                 typen = zeile.get("Typ")
                 name = zeile.get("Gemeindename")
@@ -57,15 +64,14 @@ def sendeMailMitCodes(request):
                 break
 
         if emailAdresse and stadtverwaltungName: # wenn E-Mail und Name vorhanden ist
-            csv_path = os.path.join(settings.BASE_DIR, '/var/www/django-projekt/KORA/Code.csv') # Pfad zur Datei mit den vergebenen Codes
-            dateiExistiert = os.path.isfile(csv_path) # prüft, ob die Datei bereits vorhanden ist
+            dateiExistiert = os.path.isfile(codeCSV) # prüft, ob die Datei bereits vorhanden ist
 
             # Vorhandene Passwörter und registrierte Stadtverwaltungen laden
             vergebenePasswörter = set()
             registrierteStadtverwaltungen = set()
 
             if dateiExistiert:
-                with open(csv_path, mode='r', encoding='utf-8') as datei:
+                with open(codeCSV, mode='r', encoding='utf-8') as datei:
                     # Datei als Dictionary lesen
                     reader = csv.DictReader(datei)
                     next(reader, None) # Kopfzeile überspringen
@@ -79,7 +85,7 @@ def sendeMailMitCodes(request):
 
             # Prüfen, ob die Stadtverwaltung schon registriert wurde
             if stadtverwaltungName in registrierteStadtverwaltungen:
-                return render(request, "KORA/signup.html", {
+                return render(request, "KORA/Kontoerstellung.html", {
                     "success": False,
                     "email": emailAdresse,
                     "gemeinden": gemeinden,
@@ -117,7 +123,7 @@ def sendeMailMitCodes(request):
             )
 
             # In CSV schreiben
-            with open(csv_path, mode='a', newline='', encoding='utf-8') as datei:
+            with open(codeCSV, mode='a', newline='', encoding='utf-8') as datei:
                 writer = csv.writer(datei)
                 if not dateiExistiert:
                     # Kopfzeile
@@ -127,8 +133,207 @@ def sendeMailMitCodes(request):
 
             erfolg = True
 
-    return render(request, 'KORA/signup.html', {
+    return render(request, 'KORA/Kontoerstellung.html', {
         'success': erfolg,
         'email': emailAdresse,
         'gemeinden': gemeinden,
     })
+
+
+# Funktion zur Anmeldung
+def codeÜberprüfungAnmeldung(request):
+    fehlermeldung = ""
+
+    if request.method == "POST":
+        eingegebenerCode = request.POST.get("code", "").strip()
+        if not eingegebenerCode:
+            fehlermeldung = "Bitte geben Sie einen Code ein."
+        else:
+            if os.path.isfile(codeCSV):
+                with open(codeCSV, mode="r", encoding="utf-8") as datei:
+                    reader = csv.DictReader(datei)
+                    for zeile in reader:
+                        einfach = zeile.get("Passwort", "").strip()
+                        admin = zeile.get("Admin", "").strip()
+                        stadtverwaltung = zeile.get("Stadtverwaltung", "").strip()
+                        if eingegebenerCode == einfach:
+                            request.session["stadtverwaltung"] = stadtverwaltung
+                            request.session["rolle"] = "benutzer"
+                            return redirect('Uebersicht')                            
+                        elif eingegebenerCode == admin:
+                            request.session["stadtverwaltung"] = stadtverwaltung
+                            request.session["rolle"] = "admin"
+                            return redirect("Adminverwaltung")                            
+                        
+            fehlermeldung = "Ungültiger Code. Bitte versuchen Sie es erneut."
+
+    return render(request, "KORA/Einwahl.html", {
+        "fehler": fehlermeldung
+    })
+
+
+def stockwerkeView(request):
+    stadtverwaltung = request.session.get('stadtverwaltung')
+    if not stadtverwaltung:
+        return redirect("Einwahl.html")
+    rolle = request.session['rolle']
+    if rolle == "admin":
+        return redirect("Adminverwaltung.html")
+    
+    raumdaten = []
+    with open(räumeCSV, mode='r', encoding="utf-8-sig") as file:
+         reader = csv.DictReader(file, delimiter=',')
+         for row in reader:
+             if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                raumdaten.append({
+                    'Gemeindename': row["Gemeindename"],
+                    'Raumnummer': row["Raumnummer"],
+                    'Stockwerk': row["Stockwerk"],
+                    'SensorID': row["SensorID"],
+                    'RFIDID': row["RFIDID"],
+                })
+
+    rfidDaten = {}
+    with open(rfidCSV, mode='r', encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                rfidDaten[row['RFIDID']] = {
+                    'Zustand': row['Zustand'],
+                    'ChipID': row['ChipID'],
+                }
+
+
+    for raum in raumdaten:
+        rfidID = raum['RFIDID']
+        if rfidID in rfidDaten:
+            raum['Zustand'] = rfidDaten[rfidID]['Zustand']
+            raum['ChipID'] = rfidDaten[rfidID]['ChipID']
+
+    sensorDaten = {}
+    with open(temperaturCSV, mode='r', encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                sensorDaten[row['SensorID']] = {
+                    'Temperatur': row['Temperatur'],
+                    'Luftfeuchtigkeit': row['Luftfeuchtigkeit'],
+                }
+
+    for raum in raumdaten:
+        sensorID = raum['SensorID']
+        if sensorID in sensorDaten:
+            raum['Temperatur'] = sensorDaten[sensorID]['Temperatur']
+            raum['Luftfeuchtigkeit'] = sensorDaten[sensorID]['Luftfeuchtigkeit']
+
+
+
+
+    return render(request, 'KORA/Uebersicht.html', {'raumdaten': raumdaten})
+
+
+
+def auswahlView(request):
+    stadtverwaltung = request.session.get('stadtverwaltung')
+    if not stadtverwaltung:
+        return redirect("Einwahl.html")
+    rolle = request.session.get("rolle")
+    if rolle == "admin":
+        return redirect("Adminverwaltung.html")
+
+    
+    
+    raumdaten = []
+    with open(räumeCSV, mode='r', encoding="utf-8-sig") as file:
+         reader = csv.DictReader(file, delimiter=',')
+         for row in reader:
+             if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                raumdaten.append({
+                    'Gemeindename': row["Gemeindename"],
+                    'Raumnummer': row["Raumnummer"],
+                    'Stockwerk': row["Stockwerk"],
+                    'SensorID': row["SensorID"],
+                    'RFIDID': row["RFIDID"],
+                })
+
+    rfidDaten = {}
+    with open(rfidCSV, mode='r', encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                rfidDaten[row['RFIDID']] = {
+                    'Zustand': row['Zustand'],
+                    'ChipID': row['ChipID'],
+                }
+
+    for raum in raumdaten:
+        rfidID = raum['RFIDID']
+        if rfidID in rfidDaten:
+            raum['Zustand'] = rfidDaten[rfidID]['Zustand']
+            raum['ChipID'] = rfidDaten[rfidID]['ChipID']
+
+    sensorDaten = {}
+    with open(temperaturCSV, mode='r', encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                sensorDaten[row['SensorID']] = {
+                    'Temperatur': row['Temperatur'],
+                    'Luftfeuchtigkeit': row['Luftfeuchtigkeit'],
+                }
+
+    for raum in raumdaten:
+        sensorID = raum['SensorID']
+        if sensorID in sensorDaten:
+            raum['Temperatur'] = sensorDaten[sensorID]['Temperatur']
+            raum['Luftfeuchtigkeit'] = sensorDaten[sensorID]['Luftfeuchtigkeit']
+
+    chipDaten = {}
+    with open(mitarbeiterChipCSV, mode='r', encoding="utf-8-sig") as file:
+        reader = csv.DictReader(file, delimiter=',')
+        for row in reader:
+            if "Gemeindename" in row and row["Gemeindename"].strip() == stadtverwaltung.strip():
+                chipDaten[row['ChipID']] = {
+                    'Mitarbeitername': row['Mitarbeitername'],
+                }
+
+    for raum in raumdaten:
+        chipID = raum.get('ChipID')
+        if chipID in chipDaten:
+            raum['Mitarbeitername'] = chipDaten[chipID]['Mitarbeitername']
+
+    return render(request, 'KORA/Info.html', {'raumdaten': raumdaten})
+
+
+def adminView(request):
+    stadtverwaltung = request.session.get('stadtverwaltung')
+    if not stadtverwaltung:
+        return redirect("Einwahl.html")
+    rolle = request.session.get("rolle")
+    if rolle != "admin":
+        return redirect("Uebersicht.html")
+    return render(request, 'KORA/Adminverwaltung.html')
+
+def persoanlView(request):
+    stadtverwaltung = request.session.get('stadtverwaltung')
+    if not stadtverwaltung:
+        return redirect("Einwahl.html")
+    rolle = request.session.get("rolle")
+    if rolle != "admin":
+        return redirect("Uebersicht.html")
+    return render(request, 'KORA/Personalverwaltung.html')
+
+def kontaktView(request):
+    stadtverwaltung = request.session.get('stadtverwaltung')
+    if not stadtverwaltung:
+        return redirect("Einwahl.html")
+    return render(request, 'KORA/Kontakt.html')
+
+def Abmeldung(request):
+    request.session.flush()
+
+    session_file_path = os.path.join(settings.SESSION_FILE_PATH, f'sessionid{request.session.session_key}')
+    if os.path.exists(session_file_path):
+        os.remove(session_file_path)
+    return redirect("Einwahl")
+
