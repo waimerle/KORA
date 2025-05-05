4aa9e916126d34a7232cc7d11b1cdb6c3d2533cc
waimerle <167164782+waimerle@users.noreply.github.com>
2025-05-05 14:30:34 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,6 +3,12 @@ from KORA import views as app_views
 from . import views
 
 urlpatterns = [
-	path("signup.html", app_views.sendeMailMitCodes, name='signup'),
-
+    path("Kontoerstellung.html", app_views.sendeMailMitCodes, name='Kontoerstellung'),
+    path("Einwahl.html", app_views.codeÜberprüfungAnmeldung, name='Einwahl'),
+    path("Uebersicht.html", app_views.stockwerkeView, name='Uebersicht'),
+    path("Info.html", app_views.auswahlView, name='Info'),
+    path("Adminverwaltung.html", app_views.adminView, name= 'Adminverwaltung'),
+    path("Personalverwaltung.html", app_views.persoanlView, name= 'Personalverwaltung'),
+    path("Kontakt.html", app_views.kontaktView, name='Kontakt'),
+    path("abmelden", app_views.Abmeldung, name='abmelden'),    
 ]
