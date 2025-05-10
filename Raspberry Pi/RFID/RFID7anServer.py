987144c98eceeae3957b8d6bb8524470b5f242d5
waimerle <167164782+waimerle@users.noreply.github.com>
2025-05-10 14:26:23 +0200

    Add files via upload

---
+++ Raspberry Pi/RFID/RFID7anServer.py
@@ -0,0 +1,56 @@
+import requests
+from mfrc522 import MFRC522
+import RPi.GPIO as GPIO
+import time
+import datetime
+
+reader = MFRC522(bus=0, device=1, spd=50000, pin_mode=10, pin_rst=22)
+url = "http://[2001:7c0:2320:2:f816:3eff:fec1:da44]:8000/rfid-empfang/"
+
+def send_rfid_data(gemeinde, rfid_id, chip_id, zeit, status):
+    data = {
+        "gemeinde": gemeinde,
+        "rfid_id": rfid_id,
+        "eintraege": [
+            {"zeit": zeit, "rfid": chip_id, "status": status}
+        ]
+    }
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            print(f"Erfolgreich gesendet: {chip_id} um {zeit} mit Stauts: {status}")
+        else:
+            print(f"Fehler beim Senden der Daten {response.status_code}")
+    except Exception as e:
+        print("Fehler:", e)
+
+try:
+    print("Warte auf Karten...")
+    uid_status = {}
+    while True:
+        (status, TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
+        if status == reader.MI_OK:
+            (status, uid) = reader.MFRC522_Anticoll()
+            if status == reader.MI_OK:
+                uid = ''.join([str(i) for i in uid])
+                zeit = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                gemeinde = "Bad Urach"
+                rfid_id = 2
+                # Überprüfen, ob die UID bereits im Dictionary ist
+                if uid not in uid_status:
+                    uid_status[uid] = 0  # Initialisiere den Statuszähler für die UID
+
+                # Bestimme den Status basierend auf dem Zähler
+                uid_status[uid] += 1
+                if uid_status[uid] % 2 == 1:
+                    status_text = "kommen"  # Ungerade Anzahl: kommen
+                else:
+                    status_text = "gehen"   # Gerade Anzahl: gehen
+                
+                send_rfid_data(gemeinde, rfid_id, uid, zeit, status_text)
+                time.sleep(3)
+
+except KeyboardInterrupt:
+    print("Beendet durch Benutzer.")
+finally:
+    GPIO.cleanup()
