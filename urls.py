3c36172e9952624db09167ca59518c12cbd49b52
waimerle <167164782+waimerle@users.noreply.github.com>
2025-05-29 18:09:35 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -7,10 +7,12 @@ urlpatterns = [
     path("Einwahl.html", app_views.codeÜberprüfungAnmeldung, name="Einwahl"),
     path("Uebersicht.html", app_views.übersichtRäume, name="Uebersicht"),
     path("Info.html", app_views.infoRaumbelegung, name="Info"),
-    path("Adminverwaltung.html", app_views.raumVerwaltungAdmin, name= "Adminverwaltung"),
+    path("Raumverwaltung.html", app_views.raumVerwaltungAdmin, name= "Raumverwaltung"),
     path("Personalverwaltung.html", app_views.persoanlVerwaltungAdmin, name= "Personalverwaltung"),
     path("Kontakt.html", app_views.impressum, name="Kontakt"),
     path("abmelden", app_views.abmeldung, name="abmelden"),
     path("rfid-empfang/", app_views.rfidDatenEmpfang, name="rfid_empfang"),
     path("Vorhersage.html/<str:rfid>/", app_views.vorhersage, name="Vorhersage"),
+    path("sensordaten/", app_views.dhtDatenEmpfang, name="dhtDatenEmpfang"),
+    
 ]
